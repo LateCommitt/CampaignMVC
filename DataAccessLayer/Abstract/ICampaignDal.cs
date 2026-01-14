ef7cc47d13b9f54bc91cffdcf851ba894029105f
Ali Berr Arvas <aliberr0665@gmail.com>
2026-01-15 00:17:56 +0300

    DataAccess Layer

---
+++ DataAccessLayer/Abstract/ICampaignDal.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer.Concrete;
+using EntityLayer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    interface ICampaignDal : IGenericDal<EntityLayer.Concrete.Campaign>
+    {
+    }
+}
